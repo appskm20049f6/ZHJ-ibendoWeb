4b392e49e1c1c903f9bccaaba5f78fb632959d91
Koi tw <appskm20049f6@gmail.com>
2022-12-23 14:38:31 +0800

    20221223 1438
    loading設置

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
 let loginCheck = document.querySelector("#login");
 let ibendobuttonCheck = document.querySelector("#ibendo-button");
+let loader = document.querySelector("#loader");
+let pageHide = document.querySelector("#page-1");
+
+windows.onload = loading;
+
+let loading = () => {
+  loader.style.visibility = "hidden";
+  pageHide.style.visibility = "visible";
+};
 
 let login = () => {
   loginCheck.style.display = "none";
