6079a7b795dbbfafa5b8e85225e2e7ea2f952f03
Koi tw <appskm20049f6@gmail.com>
2023-01-30 10:45:53 +0800

    2023-0130-1045

--- public/js/app.js
+++ public/js/app.js
@@ -1,43 +1,64 @@
 //全域JS事件
-let convert = (e) => {
-  if (confirm("確認是否需要購買") == true) {
-    let datadata = {
-      gameid: sessionStorage.getItem("gameId"),
-      serverid: sessionStorage.getItem("server_id"),
-      token: sessionStorage.getItem("token"),
-      exchangeid: e,
-    };
+let zhjgamdID = {
+  gameid: sessionStorage.getItem("gameId"),
+  serverid: sessionStorage.getItem("server_id"),
+  token: sessionStorage.getItem("token"),
+};
 
-    axios({
-      method: "POST",
-      baseURL: "http://localhost:3000/Exchange",
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Content-Type": "application/json",
-      },
-      data: datadata,
-    })
-      .then((res) => {
-        location.reload();
-        alert(res.data.Message);
-      })
-      .catch((err) => {
+let convert = (e) => {
+  axios({
+    method: "POST",
+    baseURL: "https://zhj.gameflier.com/service/BonusReward/api/GetExchange",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    data: zhjgamdID,
+  })
+    .then((res) => {
+      let itemname = "測試";
+      for (let index = 0; index < 3; index++) {
+        if (res.data.Data.list[index].exchangeid == e) {
+          itemname = res.data.Data.list[index].itemname;
+        }
+      }
+
+      if (confirm(`確認是否要兌換${itemname}`) == true) {
+        let datadata = {
+          gameid: sessionStorage.getItem("gameId"),
+          serverid: sessionStorage.getItem("server_id"),
+          token: sessionStorage.getItem("token"),
+          exchangeid: e,
+        };
         axios({
           method: "POST",
           baseURL: "https://zhj.gameflier.com/service/BonusReward/api/Exchange",
           headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Content-Type": "application/json",
+            "Content-Type": "application/x-www-form-urlencoded",
           },
           data: datadata,
         })
           .then((res) => {
-            location.reload();
-            alert(res.data.Message);
+            console.log(res);
+            if (res.data.Code == -99) {
+              alert(res.data.Message);
+              sessionStorage.removeItem("gameId");
+              sessionStorage.removeItem("role_id");
+              sessionStorage.removeItem("server_id");
+              sessionStorage.removeItem("role_name");
+              sessionStorage.removeItem("token");
+              document.location.href = sessionStorage.getItem("url");
+            } else if (res.data.Code == -4) {
+              alert(res.data.Message);
+            } else {
+              alert(res.data.Message);
+            }
           })
           .catch((err) => {
             console.log(err);
           });
-      });
-  }
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
